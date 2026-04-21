1e7b9bbf8f1152436999eaba7053f8aec5a7f615
cristianmonsalve14 <c.m.budrovich@gmail.com>
2026-04-20 23:18:31 -0400

    primer commit del attendanceService

---
+++ src/test/java/cl/duoc/libroDigital/attendanceService/AttendanceServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package cl.duoc.libroDigital.attendanceService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttendanceServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
